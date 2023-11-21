730cbf2ac484decd4f9d86fd747eb7ef11095e03
Yoshi2885 <yoshitaka_tanaka_aa@mail.toyota.co.jp>
2023-11-21 15:27:17 +0900

    再テスト実装

--- src/List.tsx
+++ src/List.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 import "./List.css";
 
+const dbGetURL = "http://localhost:3000/table";
+
 interface DBTable {
   id: number;
   user_id: number;
   pokemon_id: number;
+  eng_name: string;
+  jp_name: string;
+  img_url: string;
   correct_or_incorrect: boolean;
 }
 [];
@@ -12,43 +17,46 @@ function List() {
   const [data, setData] = useState<DBTable[]>([]);
 
   useEffect(() => {
-    const dbData = async () => {
-      const result = [
-        { id: 1, user_id: 1, pokemon_id: 729, correct_or_incorrect: false },
-        { id: 2, user_id: 1, pokemon_id: 595, correct_or_incorrect: false },
-        { id: 3, user_id: 1, pokemon_id: 415, correct_or_incorrect: false },
-        { id: 4, user_id: 1, pokemon_id: 594, correct_or_incorrect: false },
-        { id: 5, user_id: 1, pokemon_id: 632, correct_or_incorrect: false },
-        { id: 6, user_id: 1, pokemon_id: 229, correct_or_incorrect: true },
-        { id: 7, user_id: 1, pokemon_id: 703, correct_or_incorrect: false },
-      ];
-      setData(result);
+    const fetchDB = async () => {
+      try {
+        const response = await fetch(dbGetURL);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        setData(jsonData); // jsonDataがDBTable[]型であることを確認
+      } catch (error) {
+        console.error("データの取得に失敗しました:", error);
+      }
     };
-    dbData();
+
+    fetchDB();
   }, []);
 
   return (
     <>
-      <div>正答率75%以上のポケモンは</div>
-      <div>◯/1000匹です</div>
       <div>あなたが間違えたポケモンはこちらです</div>
       <div>
         <table>
           <thead>
             <tr>
               <th>ID</th>
-              <th>USER_ID</th>
               <th>図鑑番号</th>
-              <th>正誤結果</th>
+              <th>すがた</th>
+              <th>英語名</th>
+              <th>日本語名</th>
             </tr>
           </thead>
           <tbody>
             {data.map((item) => (
               <tr key={item.id}>
                 <td>{item.id}</td>
-                <td>{item.user_id}</td>
                 <td>{item.pokemon_id}</td>
-                <td>{item.correct_or_incorrect ? "正解" : "不正解"}</td>
+                <td>
+                  <img src={item.img_url} alt={item.jp_name} />
+                </td>
+                <td>{item.eng_name}</td>
+                <td>{item.jp_name}</td>
               </tr>
             ))}
           </tbody>
--- src/Retry.tsx
+++ src/Retry.tsx
@@ -1,32 +1,128 @@
 import { useEffect, useState } from "react";
-// import './App.css'
+import "./Test.css";
+
+const rootURL = "http://localhost:3000/";
+const postURL = "http://localhost:3000/postans";
+
+interface PokeArr {
+  id: number;
+  imgURL: string;
+  engName: string;
+  jpName: string;
+  height: number;
+  weight: number;
+  type: string;
+}
+[];
 
 function Retry() {
-  // test用
-  const fetchFunc = async () => {
-    const initURL = "https://pokeapi.co/api/v2/pokemon/";
+  const [pokemonData, setPokemonData] = useState<PokeArr[]>([]);
+  const [inputVal, setInputVal] = useState<string | null>(null);
+  const [correctFlag, setCorrectFlag] = useState<boolean | null>(null);
+
+  const getTestPokeData = async () => {
     try {
-      const res = await fetch(`http://localhost:3000/pokedict?url=${initURL}`);
-      const data = res.json();
-      console.log(data);
+      const res = await fetch(`${rootURL}retry`);
+      const data = await res.json();
+      await setPokemonData(data);
     } catch (error) {
       console.error("error");
     }
   };
+
+  const getInputVal = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputVal(e.target.value);
+  };
+
+  const resetInput = () => {
+    setInputVal(null);
+    const answerInput = document.getElementsByClassName("answer")[0];
+    if (answerInput instanceof HTMLInputElement) {
+      answerInput.value = "";
+    }
+  };
+
+  const postData = async (url: string, data: boolean | null) => {
+    try {
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          value: data,
+          pokemon_id: pokemonData[0].id,
+          img_url: pokemonData[0].imgURL,
+          eng_name: pokemonData[0].engName,
+          jp_name: pokemonData[0].jpName,
+        }),
+      });
+      if (!res.ok) {
+        throw new Error("error");
+      }
+    } catch (error) {
+      console.error("error");
+    }
+  };
+
+  const sendAns = async () => {
+    if (inputVal === null) return;
+    if (
+      inputVal === pokemonData[0].engName ||
+      inputVal === pokemonData[0].jpName
+    ) {
+      await resetInput();
+      await setCorrectFlag(true);
+      await postData(postURL, true);
+    } else {
+      await resetInput();
+      await setCorrectFlag(false);
+      await postData(postURL, false);
+    }
+  };
+
+  const reTry = () => {
+    setCorrectFlag(null);
+    resetInput();
+    getTestPokeData();
+  };
+
   useEffect(() => {
-    fetchFunc();
+    getTestPokeData();
   }, []);
+
   return (
     <>
       <div>⌛復習の時間だ⌛</div>
-      <div className="card">
-        <img src="API" alt="pokefig" />
-      </div>
-      <div>だ～れだ？</div>
-      <input type="text" />
-      <button>回答する</button>
+      <div className="top-q">だ～れだ？</div>
+      {pokemonData.length > 0 && (
+        <div className="test-card">
+          <img src={pokemonData[0].imgURL} alt="pokefig" />
+        </div>
+      )}
+      <input className="answer" type="text" onChange={getInputVal} />
+      <button onClick={sendAns}>回答する</button>
       <div>{null}</div>
       {/* 正解かハズレかを表示 */}
+      {correctFlag === null ? null : correctFlag ? (
+        <>
+          <img className="satoshi" src="/get.jpeg" alt="vite" />
+          <div>getだぜ！</div>
+        </>
+      ) : (
+        <>
+          <img className="rocket" src="/yanakanji.jpeg" alt="vite" />
+          <div>やな感じぃ〜！</div>
+          <div>
+            正解は：{pokemonData[0].engName} / {pokemonData[0].jpName} でした
+          </div>
+        </>
+      )}
+      <div>
+        {correctFlag === null ? null : (
+          <button onClick={reTry}>もう一回？</button>
+        )}
+      </div>
     </>
   );
 }
